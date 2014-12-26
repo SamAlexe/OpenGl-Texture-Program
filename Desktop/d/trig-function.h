fa2974b2417a4b6f92695236af7bab85cc832aed
master <170023@glenbrook225.org>
2014-12-26 17:42:07 -0600

    first commit

---
+++ Desktop/d/trig-function.h
@@ -0,0 +1,15 @@
+
+
+#ifndef __TRIG_-_FUNCTION_H__
+#define __TRIG_-_FUNCTION_H__
+
+class trig
+{
+    public:
+    float trigsin(int value);
+    float trigcos(int value);
+    float trigtan(int value);
+    private:
+    int value;
+};
+#endif
