e40983a37b7f9a232bfe5bb62804ec0b0577acd2
Sam Alexe <SamAlexe@users.noreply.github.com>
2014-12-26 18:18:12 -0600

    Create README.m

---
+++ README.m
@@ -0,0 +1,29 @@
+OpenGl-Texture-Program
+======================
+Created on 12/25/14 
+by Brian Huang
+
+
+Installation / Requirements:
+
+===========================
+Ubuntu OS
+
+package : 
+=========
+freeglut3-dev
+
+commands:
+========
+
+sudo apt-get install freeglut3-dev
+
+reference:
+==========
+
+-lglut - reference OpenGl include files
+
+
+MAKEFILE
+========
+make
